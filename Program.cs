50cc82ed1ef43e543abcc7b74486a84222842cdb
Venzke <67403605+Venzke@users.noreply.github.com>
2020-11-03 13:32:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_Class_Metod
+{
+    class Program
+    {
+        static void Main()
+        {
+            Retangulo x;
+
+            x = new Retangulo();
+
+            Console.Write("Insira o 1° atributo: ");
+            x.A = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira o 2° atributo: ");
+            x.B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            double Area = x.Calc_Area(); //Area recebendo o resultado do processamento do método Calc_Area da classe Retangulo.
+            Console.WriteLine("Area = {0:F2}", Area);
+
+            double Perimetro = x.Calc_Perimetro();
+            Console.WriteLine("Perimetro = {0:F2}", Perimetro);
+
+            double Diagonal = x.Calc_Diagonal();
+            Console.WriteLine("Diagonal = {0:F2}", Diagonal);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Retangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_Class_Metod
+{
+    class Retangulo
+    {
+        public double A, B;
+
+        public double Calc_Area()
+        {
+            return A * B;
+        }
+
+        public double Calc_Perimetro()
+        {
+            return 2 * ( A + B );
+        }
+
+        public double Calc_Diagonal()
+        {
+            return Math.Sqrt(A * A + B * B);
+        }
+    }
+}
